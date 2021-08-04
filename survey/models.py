a268d623b6634e4b821edad729382933cf83588d
Jihoon@RapportLabs <jihoon@rapportlabs.kr>
2021-08-05 03:29:53 +0900

    어드민 설정

--- survey/models.py
+++ survey/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Survey(models.Model):
     title = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.title
+
 
 class SurveyQuestion(models.Model):
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
@@ -23,6 +26,9 @@ class SurveyQuestion(models.Model):
 
     choice_limit = models.PositiveSmallIntegerField()
 
+    def __str__(self):
+        return self.topic
+
 
 class Option(models.Model):
     question = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
@@ -36,8 +42,14 @@ class SurveyResponse(models.Model):
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15)
 
+    def __str__(self):
+        return self.survey.title + ' 응답 결과 #' + str(self.id)
+
 
 class SurveyQuestionResponse(models.Model):
     survey_response = models.ForeignKey(SurveyResponse, on_delete=models.CASCADE)
     topic = models.CharField(max_length=50)
     selected_options = models.ManyToManyField(Option)
+
+    def __str__(self):
+        return self.topic
