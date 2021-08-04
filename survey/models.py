38e001f6db608243a6472523787eb7cac8c5b7b4
Jihoon@RapportLabs <jihoon@rapportlabs.kr>
2021-08-04 11:44:52 +0900

    survey 관련 model들 생성

--- survey/models.py
+++ survey/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Answer(models.Model):
+    text = models.CharField(max_length=200)
+
+
+class SurveyQuestion(models.Model):
+    title = models.CharField(max_length=200)
+
+    Type = models.Choices('Type', 'SELECT RADIO CHECKBOX')
+    type = models.CharField(
+        max_length=200,
+        choices=Type.choices,
+    )
+
+    required = models.BooleanField()
+
+    choice_limit = models.PositiveSmallIntegerField()
+
+    answers = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
+
+class Survey(models.Model):
+    questions = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
