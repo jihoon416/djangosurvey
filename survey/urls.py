bfe8936505f4bba49b25440ad3f93d94694ada0a
Jihoon@RapportLabs <jihoon@rapportlabs.kr>
2021-08-05 00:41:59 +0900

    survey에 대한 view 추가

--- survey/urls.py
+++ survey/urls.py
@@ -1,3 +1,7 @@
-urlpatterns = [
+from django.urls import path
 
+from survey import views
+
+urlpatterns = [
+    path('survey/<int:survey_id>/', views.survey)
 ]
--- survey/views.py
+++ survey/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-# Create your views here.
+from survey.models import Survey
+
+
+def survey(request: HttpRequest, survey_id: int):
+    if request.method == 'POST':
+        for key, value in request.POST.lists():
+            print("%s %s" % (key, value))
+    selected_survey = Survey.objects.get(id=survey_id)
+    return render(request, 'survey.html', {'survey': selected_survey})
