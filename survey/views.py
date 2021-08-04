e558f11130c3a672798937ffd85c897a47779bce
Jihoon@RapportLabs <jihoon@rapportlabs.kr>
2021-08-05 02:46:57 +0900

    전화번호 잘못 등록되는 문제 해결

--- survey/views.py
+++ survey/views.py
@@ -11,22 +11,22 @@ def survey(request: HttpRequest, survey_id: int):
     if request.method == 'POST':
         question_response_dict = {}
         survey_response = SurveyResponse(survey=selected_survey)
-        for name, option_list in request.POST.lists():
+        for name, input_list in request.POST.lists():
             if name == 'csrfmiddlewaretoken':
                 continue
 
             if name == 'tel':
-                survey_response.phone_number = option_list
+                survey_response.phone_number = input_list[0]
                 continue
 
             question = SurveyQuestion.objects.get(id=name)
 
-            if len(option_list) > question.choice_limit:
+            if len(input_list) > question.choice_limit:
                 return render(request, 'survey.html', {
                     'survey': selected_survey,
                     'error': f'선택 가능한 숫자를 넘었습니다. (문항: {question.topic})',
                 })
-            if question.required and len(option_list) < 1:
+            if question.required and len(input_list) < 1:
                 return render(request, 'survey.html', {
                     'survey': selected_survey,
                     'error': f'필수 선택 문항이 입력되지 않았습니다. (문항: {question.topic})',
@@ -36,7 +36,7 @@ def survey(request: HttpRequest, survey_id: int):
                 return question.option_set.filter(id__in=option_id_list)
 
             survey_question_response = SurveyQuestionResponse(survey_response=survey_response, topic=question.topic)
-            options = get_options(option_list)
+            options = get_options(input_list)
             question_response_dict[question.id] = survey_question_response, options
         survey_response.save()
         for question_id in question_response_dict:
